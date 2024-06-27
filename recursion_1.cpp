6c67685c6cf829be0aa7f876ab34e4528d0111ad
Bawa <7207459+bawasv@users.noreply.github.com>
2024-06-27 18:09:39 -0400

    recursion revision assignment #1

--- recursion_1.cpp
+++ recursion_1.cpp
@@ -15,110 +15,90 @@ void printCubesReverse(int n)
 {
 	if(n==1)
 	{
-		cout<<n<<" ";
-		return;
+		cout<<n*n*n<<" ";
+		printCubesReverse(n-1);
 	}
-	cout<<n*n*n<<" ";
-	printCubesReverse(n-1);
 }
 
 void printCubes(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
-		cout<<n<<" ";
-		return;
+		printCubes(n-1);
+		cout<<n*n*n<<" ";
 	}
-	printCubes(n-1);
-	cout<<n*n*n<<" ";
 }
 
 void printSquaresReverse(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
-		cout<<n<<" ";
-		return;
+		cout<<n*n<<" ";
+		printSquaresReverse(n-1);
 	}
-	cout<<n*n<<" ";
-	printSquaresReverse(n-1);
 }
 
 void printSquares(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
-		cout<<n<<" ";
-		return;
+		printSquares(n-1);
+		cout<<n*n<<" ";
 	}
-	printSquares(n-1);
-	cout<<n*n<<" ";
 }
 
 void printEvenNaturalReverse(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
 		cout<<2*n<<" ";
-		return;
+		printEvenNaturalReverse(n-1);
 	}
-	cout<<2*n<<" ";
-	printEvenNaturalReverse(n-1);
 }
 
 void printEvenNatural(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
+		printEvenNatural(n-1);
 		cout<<2*n<<" ";
-		return;
 	}
-	printEvenNatural(n-1);
-	cout<<2*n<<" ";
 }
 
 void printOddNaturalReverse(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
-		cout<<n<<" ";
-		return;
+		cout<<2*n-1<<" ";
+		printOddNaturalReverse(n-1);
 	}
-	cout<<2*n-1<<" ";
-	printOddNaturalReverse(n-1);
 }
 
 void printOddNatural(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
-		cout<<n<<" ";
-		return;
+		printOddNatural(n-1);
+		cout<<2*n-1<<" ";
 	}
-	printOddNatural(n-1);
-	cout<<2*n-1<<" ";
 }
 
 void printNaturalReverse(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
 		cout<<n<<" ";
-		return;
+		printNaturalReverse(n-1);
 	}
-	cout<<n<<" ";
-	printNaturalReverse(n-1);
 }
 
 void printNatural(int n)
 {
-	if(n==1)
+	if(n>0)
 	{
+		printNatural(n-1);
 		cout<<n<<" ";
-		return;
 	}
-	printNatural(n-1);
-	cout<<n<<" ";
 }
 
 
